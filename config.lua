1a7eaf34ad6ee0e101b638ed2fbc44f3ecf21bb0
hamshad <hiromega18@gmail.com>
2025-07-03 17:23:40 +0530

    docs: how to add lombok into jdtls

--- config.lua
+++ config.lua
@@ -41,3 +41,28 @@ vim.keymap.set('n', '<leader>O', 'O<esc>')
 vim.keymap.set('n', '<leader>d', '"_d')
 vim.keymap.set('v', '<leader>d', '"_d')
 vim.keymap.set('v', '<leader>p', '"_dP')
+
+
+-- TODO: Configure the lombok for JDTLS for Annotations Processing
+-- 1. Go to the jdtls executing python script
+-- /Users/mokshassd/.local/share/nvim/mason/packages/jdtls/bin/jdtls.py
+-- 2. Add the following lines to the exec-args list:
+-- "-javaagent:" + str(jdtls_base_path / "lombok.jar"),
+-- It would look like this:
+-- exec_args = ["-Declipse.application=org.eclipse.jdt.ls.core.id1",
+--              "-Dosgi.bundles.defaultStartLevel=4",
+--              "-Declipse.product=org.eclipse.jdt.ls.core.product",
+--              "-Dosgi.checkConfiguration=true",
+--              "-Dosgi.sharedConfiguration.area=" + shared_config_path,
+--              "-Dosgi.sharedConfiguration.area.readOnly=true",
+--              "-Dosgi.configuration.cascaded=true",
+--              "-Xms1G",
+--              "--add-modules=ALL-SYSTEM",
+--              "--add-opens", "java.base/java.util=ALL-UNNAMED",
+--              "--add-opens", "java.base/java.lang=ALL-UNNAMED",
+--              "-javaagent:" + str(jdtls_base_path / "lombok.jar")] \
+--             + known_args.jvm_arg \
+--             + ["-jar", jar_path,
+--                "-data", known_args.data] \
+--             + args
+-- 3. Save the file
